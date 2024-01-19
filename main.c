eae96f90ed277cc301a4cdfec06562806150dd13
HenriqueMelo2007 <henriquemelo0983@gmail.com>
2024-01-19 16:09:59 -0300

    feat: inclui função responsável por tornar toda a chave fornecida pelo usuário para letras maiúsculas

--- main.c
+++ main.c
@@ -3,34 +3,46 @@
 #include <stdlib.h>
 #include <string.h>
 
-char* lowerCaseKeyTransform (char key[]);
-void cipherTransformation (char text[]);
+void upperCaseKeyTransform(const char *key, char *upperCaseKey);
+void cipherTransformation(char text[]);
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]) {
   setlocale(LC_ALL, "Portuguese_Brazil");
 
-  if ( argc != 2 ) {
+  if (argc != 2) {
     printf("Defina através da interface de linha de comando uma única chave para a cifra que contenha as 26 letras do alfabeto!");
     return 1;
   }
 
   int sizeOfKey = strlen(argv[1]);
-  if ( sizeOfKey != 26 ) {
+  if (sizeOfKey != 26) {
     printf("Defina através da interface de linha de comando uma única chave para a cifra que contenha as 26 letras do alfabeto!");
     return 1;
   }
 
-  char lowerCaseKey[26] = lowerCaseKeyTransform(argv[1]);
+  char upperCaseKey[26];
+  upperCaseKeyTransform(argv[1], upperCaseKey);
+  
   char text[1000];
 
   printf("Texto simples: ");
   fgets(text, sizeof(text), stdin);
 
-
   return 0;
 }
 
-char* lowerCaseKeyTransform (char key[]) {}
+void upperCaseKeyTransform(const char *key, char *upperCaseKey) {
 
-void cipherTransformation (char text[]) {}
+  for (int i = 0; i < 26; i++) {
+    if (key[i] >= 'a' && key[i] <= 'z') {
+      char upperCaseChar = key[i] - 32;
+      upperCaseKey[i] = upperCaseChar;
+    } else {
+      upperCaseKey[i] = key[i];
+    }
+  }
+
+  upperCaseKey[26] = '\0';
+}
+
+void cipherTransformation(char text[]) {}
